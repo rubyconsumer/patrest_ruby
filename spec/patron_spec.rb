50782987d7ff01794314d24a23ed0be835671cbd
Trek Glowacki <pietrekg@umich.edu>
2009-05-05 17:14:12 -0400

    updated specs to reflect different responses seen in the wild.

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -11,16 +11,31 @@ describe Patron, "accessing account data" do
     @patron = Patron.new('123456789abcdef')
   end
   
-  it "can provide an array of checked out items" do
-    mock_http(:get, "/checkouts/123456789abcdef", 'patron_checkouts.xml')
-    @patron.items.should be_kind_of(Array)
-    @patron.items[0].should be_kind_of(Record)
+  describe 'seeing checked out items' do
+    it 'returns an array if there are many items' do
+      mock_http(:get, "/checkouts/123456789abcdef", 'patron_checkouts_with_many_items.xml')
+      @patron.items.should be_kind_of(Array)
+      @patron.items[0].should be_kind_of(Record)
+    end
     
+    it 'shows an array if there is one item' do
+      mock_http(:get, "/checkouts/123456789abcdef", 'patron_checkouts_with_one_item.xml')
+      @patron.items.should be_kind_of(Array)
+      @patron.items[0].should be_kind_of(Record)
+    end
   end
   
-  it "can provide an array of items on hold" do
-    mock_http(:get, "/holds/123456789abcdef", 'patron_holds_with_one_item.xml')
-    @patron.holds.should be_kind_of(Array)
-    @patron.holds[0].should be_kind_of(Record)
+  describe 'seeing held items' do
+    it 'returns an array if there are many items' do
+      mock_http(:get, "/holds/123456789abcdef", 'patron_holds_with_many_items.xml')
+      @patron.holds.should be_kind_of(Array)
+      @patron.holds[0].should be_kind_of(Record)
+    end
+    
+    it 'returns an array if there is one item' do
+      mock_http(:get, "/holds/123456789abcdef", 'patron_holds_with_one_item.xml')
+      @patron.holds.should be_kind_of(Array)
+      @patron.holds[0].should be_kind_of(Record)
+    end
   end
 end
